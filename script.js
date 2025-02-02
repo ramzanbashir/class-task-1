c28973002f1063aaa9574a22e03ec0320dc84cb9
MUHAMMAD RAMZAN <153890643+ramzanbashir@users.noreply.github.com>
2025-02-02 14:08:05 +0500

    class-task

---
+++ script.js
@@ -0,0 +1,45 @@
+//-----Chap1------
+
+//Question-1
+// alert("Error! Please enter a valid password");
+//Question-2
+// alert("Welcome to JS Land...\nHappy Coding!");
+//Question-3
+// alert("Welcome to JS Land...");
+//Question-4
+// alert("Happy Coding!");
+//Question-5
+// alert("Hello... I can run JS through my web browser's console");
+//Question-6
+
+//-----Chap2------
+
+//Question-1
+// var username = "Muhammad Ramzan";
+//Question-2
+// var message ="Hello World"; //alert (message);
+//Question-3
+//(a) var point1 ="Jhone Doe"; //alert(point1);
+//Question-4
+//(b)var point2 ="15 years old"; //alert(point2);
+//Question-5
+//(c)var point3 ="Certified Mobile Application Development"; //alert(point3);
+//Question-6
+//var js ="PIZZA\nPIZZ\nPIZ\nPI"; //alert(js);
+//Question-7
+// var style ="My email adress is example@example.com"; //alert(style);
+//Question-8
+//var style ="I am trying to learn from the Book A smater way to learn the Javascript"; //alert(style);
+//Question-9
+//document.write("Yha! I can write HTML content through JavaScript");
+//Question-10
+//var style ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"; //alert(style);
+
+//-----Chap3------
+
+//Question-1
+//var style ="I am 15 years old"; //alert(style);
+//Question-2
+//var style ="You have visited this site 15 times"; //alert(style);
+//Question-3
+//document.write ("Mybirth year is 1990 \n Data type of my ddeclarad variable is number");
